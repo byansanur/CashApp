0ddc365d58fdb68c31dd8f7cb7233c02bbd9e8fa
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 22:11:55 +0700

    call data list in fragment list

--- app/src/main/java/com/byinc/cashapp/presentation/list/ListCashFragment.kt
+++ app/src/main/java/com/byinc/cashapp/presentation/list/ListCashFragment.kt
@@ -1,21 +1,54 @@
 package com.byinc.cashapp.presentation.list
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
+import android.util.Log
 import android.view.View
-import android.view.ViewGroup
-import com.byinc.cashapp.R
+import androidx.fragment.app.viewModels
 import com.byinc.cashapp.base.BaseFragment
 import com.byinc.cashapp.databinding.FragmentListCashBinding
+import com.byinc.cashapp.utils.Resources
+import com.byinc.cashapp.utils.gone
+import com.byinc.cashapp.utils.visible
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ListCashFragment : BaseFragment<FragmentListCashBinding>() {
 
+    private val viewModelList : ViewModelList by viewModels()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        getDataList()
+    }
 
+    private fun getDataList() {
+        viewModelList.getAllListCash().observe(viewLifecycleOwner) {
+            when(it) {
+                is Resources.Loading -> {
+                    Log.e("TAG", "onViewCreated: loading")
+                }
+                is Resources.Success -> {
+                    Log.e("TAG", "onViewCreated: success: ${it.data}")
+                    binding.apply {
+                        if (it.data.isNullOrEmpty()) {
+                            rvListCash.gone()
+                            includedMsgLayout.root.visible()
+                        } else {
+                            rvListCash.visible()
+                            includedMsgLayout.root.gone()
+                        }
+                    }
+                }
+                is Resources.Error -> {
+                    binding.apply {
+                        rvListCash.gone()
+                        includedMsgLayout.root.visible()
+                        includedMsgLayout.tvProblemInfo.text = it.message
+                    }
+                    Log.e("TAG", "onViewCreated: error: ${it.message}")
+                }
+            }
+        }
     }
 
 }
