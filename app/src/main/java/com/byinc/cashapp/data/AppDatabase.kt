df2691c2c781067cc77330d095c5f0cbfe9e2855
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 22:40:50 +0700

    add type converter for complex data type

--- app/src/main/java/com/byinc/cashapp/data/AppDatabase.kt
+++ app/src/main/java/com/byinc/cashapp/data/AppDatabase.kt
@@ -2,10 +2,12 @@ package com.byinc.cashapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverters
 import com.byinc.cashapp.data.entity.CashEntity
 import com.byinc.cashapp.data.repository.CashEntityDao
 
 @Database(entities = [CashEntity::class], version = 1, exportSchema = false)
+@TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun cashEntityDao(): CashEntityDao
 }
