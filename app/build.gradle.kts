2aef31c7bdeb51d0fd9b1d15adf503544f44658e
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 13:50:31 +0700

    setup config prod/dev environment

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,25 +5,60 @@ plugins {
 
 android {
     namespace = "com.byinc.cashapp"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.byinc.cashapp"
         minSdk = 24
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    flavorDimensions.add("cash")
+
+    productFlavors {
+        create("development") {
+            dimension = "cash"
+            applicationId = ConfigurationDev.applicationId
+            versionCode = ConfigurationDev.versionCode
+            versionName = ConfigurationDev.versionName
+            resValue("string", "app_name", ConfigurationDev.appName)
+        }
+        create("production") {
+            dimension = "cash"
+            applicationId = ConfigurationProd.applicationId
+            versionCode = ConfigurationProd.versionCode
+            versionName = ConfigurationProd.versionName
+            resValue("string", "app_name", ConfigurationProd.appName)
+        }
+    }
+
+    signingConfigs {
+        create("prodKey") {
+            enableV1Signing = false
+            enableV2Signing = true
+            enableV3Signing = true
+        }
+    }
+
     buildTypes {
         release {
-            isMinifyEnabled = false
+            signingConfig = signingConfigs["prodKey"]
+            isDebuggable = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
+        }
+        debug {
+            isDebuggable = true
+            isMinifyEnabled = false
+            isShrinkResources = false
         }
     }
     compileOptions {
@@ -33,13 +68,16 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        viewBinding = true
+    }
 }
 
 dependencies {
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.google.android.material:material:1.13.0-alpha01")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
---
+++ buildSrc/src/main/java/Configuration.kt
@@ -0,0 +1,13 @@
+object ConfigurationProd {
+    const val applicationId = "com.byinc.cashapp"
+    const val versionCode = 1
+    const val versionName = "1.0.0-beta"
+    const val appName = "MyCash"
+}
+
+object ConfigurationDev {
+    const val applicationId = "com.byinc.cashappdev"
+    const val versionCode = 1
+    const val versionName = "1.0.0-dev"
+    const val appName = "MyCash Dev"
+}
