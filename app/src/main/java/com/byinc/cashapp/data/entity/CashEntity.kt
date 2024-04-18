a6f5761201776b3732027fadd036d8a9673d3b10
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 22:08:24 +0700

    mapper entity to model class

--- app/src/main/java/com/byinc/cashapp/data/entity/CashEntity.kt
+++ app/src/main/java/com/byinc/cashapp/data/entity/CashEntity.kt
@@ -3,6 +3,7 @@ package com.byinc.cashapp.data.entity
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import com.byinc.cashapp.domain.model.CashModel
 
 @Entity(tableName = "cash_entity")
 data class CashEntity(
@@ -20,4 +21,14 @@ data class CashEntity(
     val date: String,
     @ColumnInfo(name = "time")
     val time: String
-)
+) {
+    fun toCashModel() = CashModel(
+        id = this.id,
+        name = this.name,
+        fromSource = this.fromSource,
+        amount = this.amount,
+        note = this.note,
+        date = this.date,
+        time = this.time
+    )
+}
