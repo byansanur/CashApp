51cf099f4e8ac5f10e1d28cc99791ff596f6da1d
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 22:10:55 +0700

    add ViewModelList

---
+++ app/src/main/java/com/byinc/cashapp/presentation/list/ViewModelList.kt
@@ -0,0 +1,38 @@
+package com.byinc.cashapp.presentation.list
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.byinc.cashapp.domain.DomainRepository
+import com.byinc.cashapp.domain.model.CashModel
+import com.byinc.cashapp.utils.Resources
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ViewModelList @Inject constructor(
+    private val domainRepository: DomainRepository
+) : ViewModel() {
+
+    fun getAllListCash() : LiveData<Resources<List<CashModel>>> {
+        val output = MutableLiveData<Resources<List<CashModel>>>()
+        viewModelScope.launch {
+            output.postValue(Resources.Loading())
+            delay(2000)
+            val cashModel = domainRepository.getAllCashIn()
+            if (cashModel.isNullOrEmpty()) {
+                output.postValue(Resources.Error("No Data Found!"))
+            } else {
+                cashModel.forEach {
+                    output.postValue(Resources.Success(listOf(it.toCashModel())))
+                }
+            }
+        }
+        return output
+    }
+
+
+}
