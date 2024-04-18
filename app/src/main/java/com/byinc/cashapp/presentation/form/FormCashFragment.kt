2c235395f0131ca7e9114730dd76f985e01e3c5d
Ratbyansa Nur <byan@sgo.co.id>
2024-04-18 18:59:02 +0700

    add fragment for views app

---
+++ app/src/main/java/com/byinc/cashapp/presentation/form/FormCashFragment.kt
@@ -0,0 +1,20 @@
+package com.byinc.cashapp.presentation.form
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.byinc.cashapp.R
+import com.byinc.cashapp.base.BaseFragment
+import com.byinc.cashapp.databinding.FragmentFormCashBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class FormCashFragment : BaseFragment<FragmentFormCashBinding>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
---
+++ app/src/main/java/com/byinc/cashapp/presentation/list/ListCashFragment.kt
@@ -0,0 +1,21 @@
+package com.byinc.cashapp.presentation.list
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.byinc.cashapp.R
+import com.byinc.cashapp.base.BaseFragment
+import com.byinc.cashapp.databinding.FragmentListCashBinding
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class ListCashFragment : BaseFragment<FragmentListCashBinding>() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
